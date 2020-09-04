b6a8cfe5f26e73e870388c8f9360b74be9312068
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-09-04 18:08:53 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -38,13 +38,9 @@ end
 # Define your play method below
 def play(board)
   counter = 0
-  loop do
-    counter = counter + 1
-    turn(board)
-  end
-  
-  if counter >= 9
-    break
-  end
+  while counter <= 9
+  turn(board)
+  counter += 1
+ end
   
 end
